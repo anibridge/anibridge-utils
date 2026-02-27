d92255bcd41d95163811e80f34962091486efd31
Elias Benbourenane <eliasbenbourenane@gmail.com>
2026-02-27 04:22:26 +0000

    refactor: Don't re-export packages, the sub-module should be used

--- src/anibridge/utils/__init__.py
+++ src/anibridge/utils/__init__.py
@@ -1,6 +1,1 @@
 """AniBridge shared utilities."""
-
-__all__ = ["MappingDescriptor", "ProviderLogger", "ProviderRegistry"]
-
-from anibridge.utils.registry import ProviderRegistry
-from anibridge.utils.types import MappingDescriptor, ProviderLogger

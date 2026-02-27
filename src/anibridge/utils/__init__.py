164f4ae0b399b90df58d1090eed2f9bdf0dcb854
Elias Benbourenane <eliasbenbourenane@gmail.com>
2026-02-27 03:56:14 +0000

    refactor: Re-export at root

--- src/anibridge/utils/__init__.py
+++ src/anibridge/utils/__init__.py
@@ -1,1 +1,12 @@
 """AniBridge shared utilities."""
+
+__all__ = [
+    "MappingDescriptor",
+    "ProviderLogger",
+    "ProviderRegistry",
+    "provider",
+    "registry",
+]
+
+from anibridge.utils.registry import ProviderRegistry, provider
+from anibridge.utils.types import MappingDescriptor, ProviderLogger
